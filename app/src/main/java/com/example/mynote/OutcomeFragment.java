b310365332765e9c3af37c9777a87ac17022785d
unknown <2580814416@qq.com>
2021-05-31 08:21:40 +0800

    report

---
+++ app/src/main/java/com/example/mynote/OutcomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.mynote;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OutcomeFragment extends BaseRecordFragment {
+
+
+    // 重写
+    @Override
+    public void loadDataToGV() {
+        super.loadDataToGV();
+        //获取数据库当中的数据源
+        List<TypeBean> outlist = DBManager.getTypeList(0);
+        typeList.addAll(outlist);
+        adapter.notifyDataSetChanged();
+        typeTv.setText("其他");
+        typeIv.setImageResource(R.mipmap.ic_qita_fs);
+    }
+
+    @Override
+    public void saveAccountToDB() {
+        accountBean.setKind(0);
+        DBManager.insertItemToAccounttb(accountBean);
+    }
+}
---
+++ app/src/main/java/com/example/mynote/UniteApp.java
@@ -0,0 +1,13 @@
+package com.example.mynote;
+
+import android.app.Application;
+
+/* 表示全局应用的类*/
+public class UniteApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 初始化数据库
+        DBManager.initDB(getApplicationContext());
+    }
+}
